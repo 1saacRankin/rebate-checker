7ab6a0a2ceef2058484db43577c579f1378c95d0
Isaac Rankin <56332858+1saacRankin@users.noreply.github.com>
2025-11-12 11:13:08 -0800

    Update run_rebate_checker.sh

--- run_rebate_checker.sh
+++ run_rebate_checker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Load conda
-source /Users/isaacrankin/anaconda3/etc/profile.d/conda.sh
+source /Users/path/anaconda3/etc/profile.d/conda.sh
 
-# Activate your scrape environment
+# Activate your scrape environment (typical web scraping libraries)
 conda activate scrape
 
 # Run the Python script
-python /Users/isaacrankin/EbikeRebate/rebate_checker.py
+python /Users/path to your directory/EbikeRebate/rebate_checker.py
